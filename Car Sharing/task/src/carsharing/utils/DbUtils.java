464946ac625577a85f7d5d98209d13f809e54ad1
mishoto <mihail.dev.io@gmail.com>
2022-12-11 16:41:00 +0200

    replace hardcoded values with constants

--- Car Sharing/task/src/carsharing/utils/DbUtils.java
+++ Car Sharing/task/src/carsharing/utils/DbUtils.java
@@ -5,30 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static java.lang.System.*;
+
 public final class DbUtils {
 
     private DbUtils() {
     }
 
     public static void connectToH2FileDb() {
-        final String FILE_PATH = "D:\\0_SoftServe_Academy\\Car Sharing\\Car Sharing\\task\\src\\carsharing\\db\\carsharing";
-        final String DATABASE_URL = "jdbc:h2:" + FILE_PATH;
-        try (Connection conn = DriverManager.getConnection(DATABASE_URL, "", "");
+        final String filePath = Constants.H2FileDb.FILE_PATH;
+        final String dbURL = Constants.H2FileDb.DB_PREFIX_URL + filePath;
+        final String user = Constants.H2FileDb.DB_USER;
+        final String password = Constants.H2FileDb.DB_PASSWORD;
+
+        try (Connection conn = DriverManager.getConnection(dbURL, user, password);
              Statement statement = conn.createStatement()) {
             conn.setAutoCommit(true);
-            statement.execute(
-                    "CREATE TABLE IF NOT EXISTS COMPANY (" +
-                            "ID INT auto_increment primary key," +
-                            "NAME VARCHAR(128) NOT NULL UNIQUE" +
-                            ")");
+            statement.execute(Constants.SqlInitialQueries.CREATE_COMPANY_TABLE);
 
-
-            if (conn != null) {
-                System.out.println("Connection to database successful");
-            }
+            out.println("Connection to database successful");
 
         } catch (SQLException sqlException) {
-            System.err.println("An Error occurred while connecting to database!");
+            err.println("An Error occurred while connecting to database!");
             sqlException.printStackTrace();
         }
     }
