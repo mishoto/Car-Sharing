d4fc11617c7a417cf30bf1f63dd1ecee3a9da791
mishoto <mihail.dev.io@gmail.com>
2022-12-11 16:56:17 +0200

    add method's implementation

--- Car Sharing/task/src/carsharing/models/MenuLogIn.java
+++ Car Sharing/task/src/carsharing/models/MenuLogIn.java
@@ -1,5 +1,7 @@
 package carsharing.models;
 
+import static java.lang.System.out;
+
 public class MenuLogIn extends Menu{
 
     public MenuLogIn(Display display) {
@@ -8,16 +10,41 @@ public class MenuLogIn extends Menu{
 
     @Override
     void onNext(int option) {
+        switch (option) {
+            case 0: {
+                onBack();
+                break;
+            }
+            case 1: {
+                display.setDisplay(new MenuCompanyList(display));
 
+                break;
+            }
+            case 2: {
+                display.setDisplay(new MenuCompanyCreate(display));
+
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("No such menu option");
+        }
     }
 
     @Override
     void onBack() {
-
+        display.setDisplay(new MenuStart(display));
     }
 
     @Override
     void print() {
-
+        StringBuilder sb = new StringBuilder();
+        sb.append("1. Company list\n" +
+                "2. Create a company\n" +
+                "0. Back\n" +
+                ">  ");
+        out.print(sb);
+        int choice = scanner.nextInt();
+        out.println();
+        onNext(choice);
     }
 }
