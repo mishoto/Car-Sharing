88bcc4cc6b205c2d6afb153d7bf8044a296e0ab8
mishoto <mihail.dev.io@gmail.com>
2022-12-12 14:31:31 +0200

    update constants with queries for cars

--- Car Sharing/task/src/carsharing/utils/Constants.java
+++ Car Sharing/task/src/carsharing/utils/Constants.java
@@ -18,10 +18,20 @@ public final class Constants {
         private SqlInitialQueries(){}
 
         public static final String CREATE_COMPANY_TABLE =
-                "CREATE TABLE IF NOT EXISTS company (" +
-                "id INT AUTO_INCREMENT PRIMARY KEY," +
-                "name VARCHAR(128) NOT NULL UNIQUE" +
-                ")";
+                "CREATE TABLE IF NOT EXISTS company " +
+                        "(" +
+                        "id INT AUTO_INCREMENT PRIMARY KEY," +
+                        "name VARCHAR(128) NOT NULL UNIQUE" +
+                        ")";
+
+        public static final String CREATE_CAR_TABLE =
+                "CREATE TABLE IF NOT EXISTS car " +
+                        "(" +
+                        "id INT AUTO_INCREMENT PRIMARY KEY," +
+                        "name VARCHAR(128) NOT NULL UNIQUE" +
+                        "company_id INT NOT NULL" +
+                        "FOREIGN KEY (company_id) REFERENCES Company(id)" +
+                        ")";
     }
 
     public static final class SqlCompanyQueries{
@@ -31,9 +41,30 @@ public final class Constants {
                 "INSERT INTO company (name) " +
                         "VALUES (?)";
 
-        public static final String GET_ALL_COMPANIES_PAGINATED =
+        public static final String GET_ALL_COMPANIES =
                 "SELECT name FROM company " +
                         "ORDER BY id ASC ";
 
+        public static final String GET_COMPANY_BY_ID =
+                "SELECT name FROM company " +
+                        "WHERE id = ? ";
+
+    }
+
+    public static final class SqlCarQueries{
+        private SqlCarQueries(){}
+
+        public static final String CREATE_CAR =
+                "INSERT INTO car (name, company_id) " +
+                "VALUES (?, ?)";
+
+        public static final String GET_ALL_CARS =
+                "SELECT name FROM car " +
+                        "ORDER BY id ASC ";
+
+        public static final String GET_ALL_CARS_BY_COMPANY =
+                "SELECT name FROM car " +
+                        "WHERE company_id = ? " +
+                        "ORDER BY id ASC ";
     }
 }
