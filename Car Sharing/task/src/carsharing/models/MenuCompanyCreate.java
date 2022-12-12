2e8d61ee00884cd12f6bb421e6406f93199f50dd
mishoto <mihail.dev.io@gmail.com>
2022-12-12 09:03:25 +0200

    add implementation with DAO instance

--- Car Sharing/task/src/carsharing/models/MenuCompanyCreate.java
+++ Car Sharing/task/src/carsharing/models/MenuCompanyCreate.java
@@ -1,23 +1,35 @@
 package carsharing.models;
 
+import carsharing.repository.Dao;
+import carsharing.repository.company.CompanyJDBCDao;
+
+import static java.lang.System.*;
+
 public class MenuCompanyCreate extends Menu{
+    private final Dao<Company> companyDao;
 
     public MenuCompanyCreate(Display display) {
         super(display);
+        companyDao = new CompanyJDBCDao();
     }
 
     @Override
     void onNext(int option) {
-
+        onBack();
     }
 
     @Override
     void onBack() {
-
+        display.setDisplay(new MenuLogIn(display));
     }
 
     @Override
     void print() {
-
+        out.println("Enter the company");
+        String[] newCompanyInfo = scanner.nextLine().split(" ");
+        Company newCompany = new Company(newCompanyInfo[0]);
+        companyDao.save(newCompany);
+        out.println();
+        onNext(0);
     }
 }
--- Car Sharing/task/src/carsharing/models/MenuCompanyList.java
+++ Car Sharing/task/src/carsharing/models/MenuCompanyList.java
@@ -1,23 +1,45 @@
 package carsharing.models;
 
+import carsharing.repository.Dao;
+import carsharing.repository.company.CompanyJDBCDao;
+
+import java.util.Iterator;
+
+import static java.lang.System.*;
+
 public class MenuCompanyList extends Menu{
+    private final Dao<Company> companyDao;
 
     public MenuCompanyList(Display display) {
         super(display);
+        companyDao = new CompanyJDBCDao();
     }
 
     @Override
     void onNext(int option) {
-
+        onBack();
     }
 
     @Override
     void onBack() {
-
+        display.setDisplay(new MenuLogIn(display));
     }
 
     @Override
     void print() {
-
+        if(companyDao.getAll().isEmpty()){
+            out.println("The company list is empty!");
+            out.println();
+            onBack();
+        }else{
+            Iterator<Company> companyIterator = companyDao.getAll().iterator();
+            int idx = 1;
+            while(companyIterator.hasNext()){
+                out.println(idx + ". " + companyIterator.next().getName());
+                idx++;
+            }
+            out.println();
+            onNext(0);
+        }
     }
 }
